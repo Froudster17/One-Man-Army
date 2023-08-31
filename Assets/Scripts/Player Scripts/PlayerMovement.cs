1e05c59604fb187cf56f173ab3a0154b9dcb99c5
Froudster17 <thomasfroud10@gmail.com>
2023-08-31 13:08:39 +0100

    Added rolling ability

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -8,11 +8,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rigidbody2D;
     [SerializeField] private Animator animator;
     [SerializeField] private int moveSpeed;
+    private float activeMoveSpeed;
+    public float dashSpeed;
 
-    private void FixedUpdate()
+    private float dashCounter;
+    private float dashCoolCounter;
+    public float dashlengh = .5f, dashCooldown = 1f;
+
+    private void Start()
+    {
+        activeMoveSpeed = moveSpeed;
+    }
+
+    private void Update()
     {
         playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rigidbody2D.velocity = playerInput.normalized * moveSpeed;
+        rigidbody2D.velocity = playerInput.normalized * activeMoveSpeed;
 
         animator.SetFloat("Velocity", playerInput.sqrMagnitude);
 
@@ -27,6 +38,32 @@ public class PlayerMovement : MonoBehaviour
             Vector3 newScale = transform.localScale;
             newScale.x = 1;
             transform.localScale = newScale;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashlengh;
+                animator.SetTrigger("Roll");
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
         }
     }
 }
