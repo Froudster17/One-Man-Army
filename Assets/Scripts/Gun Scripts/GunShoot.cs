957843fb709238f4f45adfc5f5b297591af96fe1
Froudster17 <thomasfroud10@gmail.com>
2023-08-31 13:25:46 +0100

    Added shot delay

--- Assets/Scripts/Gun Scripts/GunShoot.cs
+++ Assets/Scripts/Gun Scripts/GunShoot.cs
@@ -7,14 +7,18 @@ public class GunShoot : MonoBehaviour
     [SerializeField] private int ammo;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject bulletPrefab;
-
     [SerializeField] private float bulletForce = 20f;
+    [SerializeField] private float shotDelay = 0.2f; // Delay between shots
+
+    [SerializeField] private bool canShoot = true;
 
     private void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if ((Input.GetButtonDown("Fire1") || Input.GetButton("Fire1")) && canShoot)
         {
+            Debug.Log("Gun Fired");
             Shoot();
+            StartCoroutine(ShotDelay());
         }
     }
 
@@ -23,5 +27,12 @@ public class GunShoot : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+    }
+
+    private IEnumerator ShotDelay()
+    {
+        canShoot = false;
+        yield return new WaitForSeconds(shotDelay);
+        canShoot = true;
     }
 }
