98aa5819c6de6800c03f4df006b1fd19ba786363
Froudster17 <thomasfroud10@gmail.com>
2023-09-01 15:28:45 +0100

    Added health bar display for enemy's

--- Assets/Scripts/Enemy Scripts/EnemyHealth.cs
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int health;
     [SerializeField] private EnemyAnimations enemyAnimations;
+    [SerializeField] private Slider healthBar;
+
+    private void Start()
+    {
+        healthBar.gameObject.SetActive(false); // Hide the health bar on start
+
+        healthBar.maxValue = health;
+        healthBar.value = health;
+    }
 
     public void TakeDamage(int amount)
     {
+        if (!healthBar.gameObject.activeSelf)
+        {
+            healthBar.gameObject.SetActive(true); // Show the health bar when taking damage
+        }
+
         health -= amount;
-        FindObjectOfType<AudioManager>().Play("Slime Hit");
+        healthBar.value = health;
+
+        AudioManager audioManager = FindObjectOfType<AudioManager>();
+        if (audioManager != null)
+        {
+            audioManager.Play("Slime Hit");
+        }
+
         if (health <= 0)
         {
             StartCoroutine(enemyAnimations.Death());
